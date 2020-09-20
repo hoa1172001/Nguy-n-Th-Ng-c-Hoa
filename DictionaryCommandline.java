ad8a06a02bf934b1079b313c9a0b5f991120c91e
hoa1172001 <71603074+hoa1172001@users.noreply.github.com>
2020-09-21 00:23:53 +0700

    Add files via upload

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -1,4 +1,18 @@
-import java.util.
-public class DictionaryCommandline {
 
+public class DictionaryCommandline {
+    private static DictionaryManagement dictionaryManagement = new DictionaryManagement();
+    private static Dictionary dictionary = new Dictionary();
+
+    public static void showAllWord() {
+        System.out.println("No\t|English\t|Vietnam");
+        int i = 1;
+        for (Word word : dictionary.getList()) {
+            System.out.println(i + "\t|" + word.getWord_target() + "\t|" + word.getWord_explain());
+        }
+    }
+
+    public static void dictionaryBasic() {
+        dictionaryManagement.insertFromCommandline();
+        showAllWord();
+    }
 }
